86e341cc6870ed1f7b47b05ade5e7982a655bf63
sabrinaelee <67843130+sabrinaelee@users.noreply.github.com>
2021-05-06 13:04:19 -0400

    162c8e4

---
+++ _nuxt/static/1620319925/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fupbudget","\u002Fartwork","\u002Fabout","\u002Ftalk-project"]})
---
+++ _nuxt/static/1620319925/talk-project/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/talk-project", {data:[{}],fetch:{},mutations:void 0});
