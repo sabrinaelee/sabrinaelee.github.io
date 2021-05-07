d94a8c77f590420c32b7e5e120cf039b77be0868
sabrinaelee <67843130+sabrinaelee@users.noreply.github.com>
2021-05-07 13:57:19 -0400

    update site

--- dist/_nuxt/static/1620403033/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fupbudget","\u002Fartwork","\u002F","\u002Fabout","\u002Ftalk-project"]})
---
+++ dist/_nuxt/static/1620410215/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fartwork","\u002Fabout","\u002Fupbudget","\u002Ftalk-project"]})
